4d06b684078b6704af2f52481920f862730e2272
Faraz_Dev <142490970+FarazFawzeer@users.noreply.github.com>
2023-08-18 21:01:34 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("myForm").style.backgroundColor = "red";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
